d8e78a6d32285a76b9fac4fbdf7877a3c35e826d
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-18 17:27:18 +0100

    Connect GitHub Pages lesson viewer to deployed Google Apps Script API

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // 1. Deploy apps-script/Code.gs as a Google Apps Script Web App.
 // 2. Paste the /exec deployment URL below.
 // Until then, the site automatically uses the bundled static lesson snapshot.
-window.LESSON_API_URL = "";
+window.LESSON_API_URL = "https://script.google.com/macros/s/AKfycbxDaT73zIeDj2TwjbWRr-pbPrN8xVJCqusn1WHgVzNCu1leKh3T9P7RK_XINC_txQ5t/exec";
