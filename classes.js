af80d31393ea1ba924a9abb82e16f8b081bafb2e
Ayo Richard ABE [gODtECH] <ayoabe24@gmail.com>
2026-09-18 21:43:15 +0100

    Add class configuration foundation for multi-class lesson platform

---
+++ classes.js
@@ -0,0 +1,18 @@
+// LessonHub class configuration
+// This is the first layer before teachers enter lesson notes.
+
+window.LESSON_CLASSES = [
+  "Nursery 1",
+  "Nursery 2",
+  "Basic 1",
+  "Basic 2",
+  "Basic 3",
+  "Basic 4",
+  "Basic 5"
+];
+
+// Future expansion:
+// - JSS 1
+// - JSS 2
+// - JSS 3
+// - Multiple master Google Docs
